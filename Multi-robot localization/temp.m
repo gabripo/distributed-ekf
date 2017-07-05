76b341022a2d9d2e559380d81d809b3812ae5045
gabripo <gabripo93+github@gmail.com>
2017-07-05 19:16:15 +0200

    "Magic solution (to not use if you are Gianvo)"

--- Multi-robot localization/temp.m
+++ Multi-robot localization/temp.m
@@ -1,18 +1,20 @@
-clc
-clear all
-n = 6;
-x = rand(1, n);
+function increments = temp(x_tot, vecNUm)
 
-comb = nchoosek(n, 2);
+% Combinations number (without repetitions)
+comb = nchoosek(vecNUm, 2);
 
+% Increments initialization
+DX = []; DY = []; DT = [];
 
-%     for j=1:n-1
-%         for k=0:n-j
-%             x(n-k) - x(n-k-j)
-%         end
-%     end
-d = [];
-for i=1:n
-   d = [d, x(i+1:end) - x(1:end-i)];
+% Sub-vectors initialization extraction
+x = x_tot(1:3:end);
+y = x_tot(2:3:end);
+t = x_tot(3:3:end);
+
+for i=1:comb
     
+   DX = [DX, x(i+1:end) - x(1:end-i)];
+   DY = [DY, y(i+1:end) - y(1:end-i)];
+   DT = [DT, t(i+1:end) - t(1:end-i)];
 end
+increments = [DX; DY; DT];
